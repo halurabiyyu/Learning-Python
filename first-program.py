7c3e73cb2b09b4146676d5ca37040ba239531432
halurabiyyu <halurabiyyu@gmail.com>
2024-01-02 18:48:39 +0700

    Variabel, Addition, and Input

--- first-program.py
+++ first-program.py
@@ -1,1 +1,14 @@
+#first code
 print("Hello World")
+
+# Variabel
+greeting = 'Hello World'
+print(greeting)
+
+# addition
+addition = 2+2
+result = addition-1
+print("addition =", result)
+
+# input
+name = input('Masukan nama Anda: ')
