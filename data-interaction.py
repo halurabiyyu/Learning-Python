55dac626a70dd543b99dcd2ef58d9b7e2502f5ce
halurabiyyu <halurabiyyu@gmail.com>
2024-01-02 20:28:43 +0700

    Data Type

--- data-interaction.py
+++ data-interaction.py
@@ -8,3 +8,45 @@ print(type(salary))
 # boolean
 x = True
 print(type(x))
+
+x = False
+print(type(x))
+
+# String
+y = 'Dicoding'
+print(type(y))
+
+
+# String Multi Line
+multi_line = """Halo!
+Kapan terakhir kali kita bertemu?
+Kita bertemu hari Jum'at yang lalu."""
+print(multi_line)
+# output : Halo!
+# Kapan terakhir kali kita bertemu?
+# Kita bertemu hari Jum'at yang lalu.
+
+# Indexing
+d = 'dicoding'
+print(d[0])
+# output : d
+
+# Slicing
+d = 'dicoding'
+print(d[2:])
+# output : coding
+
+# Formatting
+name = "Halur"
+print(f"Hello, Nama saya {name}")
+# output : Hello, Nama saya Halur
+
+# %-Formatting
+name = "Perseus Evans"
+print("Nama Saya %s" % (name))
+# output : Nama saya Perseus Evans
+
+# str.format()
+print("Nama Saya {}" .format(name))
+# output : Nama saya Perseus Evans
+
